bb4072382a9cf7a27f277c8ca3d63889561ef80a
element222 <fabian.machicao@gmail.com>
2020-03-25 08:24:20 -0400

    actualizando consulta 5

--- CONSULTAS/Consulta 5.sql
+++ CONSULTAS/Consulta 5.sql
@@ -1,5 +1,5 @@
 -- ASeguradosQueDebenGestionarSuIndependencia
 Select persona.Nombre1, persona.Nombre2, ApellidoPaterno, ApellidoMaterno, 
 year(DATE_SUB(NOW(),INTERVAL year(persona.fechaDeNAcimiento) YEAR)) as edad from persona
-inner join asegurado on (persona.CI = asegurado.CI and asegurado.CI<>0 
+inner join asegurado on (persona.CI = asegurado.CI and asegurado.CIDependiente<>0 
 and 24<year(DATE_SUB(NOW(),INTERVAL year(persona.fechaDeNAcimiento) YEAR)));
